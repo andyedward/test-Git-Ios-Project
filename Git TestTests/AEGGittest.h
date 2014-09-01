99915494bebfbe5089cb6092dd29c14233622b5e
Andy Edward Gunawan <83andyedward@gmail.com>
2014-09-01 13:25:01 +0800

    added a test class

---
+++ Git TestTests/AEGGittest.h
@@ -0,0 +1,13 @@
+//
+//  AEGGittest.h
+//  Git Test
+//
+//  Created by Andy Edward on 1/9/14.
+//  Copyright (c) 2014 AEG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AEGGittest : NSObject
+
+@end
---
+++ Git TestTests/AEGGittest.m
@@ -0,0 +1,13 @@
+//
+//  AEGGittest.m
+//  Git Test
+//
+//  Created by Andy Edward on 1/9/14.
+//  Copyright (c) 2014 AEG. All rights reserved.
+//
+
+#import "AEGGittest.h"
+
+@implementation AEGGittest
+
+@end
